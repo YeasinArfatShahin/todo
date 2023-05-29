8cf2ac3ed1c9a294bdc2056f35a1c33a7099caae
sway16 <dawsingsh@gmail.com>
2023-05-29 14:47:51 +0600

    dawsing1

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
-      title: 'To Do app1',
+      title: 'To Do app test',
       home: const SplashScreen(),
     );
   }
